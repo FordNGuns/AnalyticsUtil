3593834bc207743839a1af23b79796c497898594
FordNGuns <76829210+FordNGuns@users.noreply.github.com>
2024-11-19 15:59:21 -0500

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -6,13 +6,15 @@
 	- GetFollowers: Retrieves the number of followers for a given user ID.
 	- GetLikes: Retrieves the number of likes for a given universe ID.
 	- GetSuffix: Retrieves the number you input as a shortened version, E.G 1.5k.
+	- GetFormat: Formats a number with commas as thousand separators.
 
 	```lua
 	local isFollowing = AnalyticsUtil.GetIsFollowing(1318471639, 86107548) (is 86107548 following 1318471639)
 	local followers = AnalyticsUtil.GetFollowers(123456789)
 	local likes = AnalyticsUtil.GetLikes(987654321)
 	local suffix = AnalyticsUtil.GetSuffix(1000000)
-	print("Is Following:", isFollowing, "Followers:", followers, "Likes:", likes, "1 Million:", suffix)
+	local formattedNumber = AnalyticsUtil.GetFormat(9000000)
+    	print("Is Following:", isFollowing, "Followers:", followers, "Likes:", likes, "1 Million:", suffix, "Formatted Number:", formattedNumber)
 	```
 ]=]
 local AnalyticsUtil = {
@@ -20,6 +22,7 @@ local AnalyticsUtil = {
 	GetFollowers =  require(script.GetFollowers);
 	GetLikes =  require(script.GetLikes);
 	GetSuffix = require(script.GetSuffix);
+	GetFormat = require(script.GetFormat);
 }
 
 type AnalyticsUtil = {
@@ -27,6 +30,7 @@ type AnalyticsUtil = {
 	GetFollowers: (UserId: number) -> number,
 	GetLikes: (UniverseId: number) -> number,
 	GetSuffix: (Number: number) -> number,
+	GetFormat: (number: number) -> number,
 }
 
 return AnalyticsUtil:: AnalyticsUtil
